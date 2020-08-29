f7b3ad37798d4ec43a13b1bbe2afc04ab7b8d4f8
Michael Vaganov <michael.vaganov@gmail.com>
2020-08-28 18:44:17 -0700

    added shooting mechanic
    * created a velocity script
    * put a shooting transform on the character's head
    * using the spae bar

---
+++ platformer3d/Assets/MrV/Scripts/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+	public GameObject thingToSpawn;
+
+	public KeyCode key = KeyCode.Space;
+
+    void Update()
+    {
+		if (Input.GetKeyDown(key))
+		{
+			Instantiate(thingToSpawn, transform.position, transform.rotation);
+		}
+    }
+}
