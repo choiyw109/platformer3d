4de404afbbfa99ec749d5cfdcaa749a709f9ce19
Michael Vaganov <michael.vaganov@gmail.com>
2020-09-27 14:32:05 -0700

    better world generation
    now a string can  be provided to act as the map

--- platformer3d/Assets/MrV/Scripts/TiledWorldGenerator.cs
+++ platformer3d/Assets/MrV/Scripts/TiledWorldGenerator.cs
@@ -4,35 +4,49 @@ using UnityEngine;
 
 public class TiledWorldGenerator : MonoBehaviour
 {
-	public GameObject[] tiles;
+	[TextArea(3,10)]
+	public string map;
+/*
+....
+.f#.
+.ff.
+....
+.  .
+  . 
+    
+.  .
+*/
+	public Vector3Int size = new Vector3Int(4, 2, 4);
+
+	[System.Serializable]
+	public class TileSource {
+		public string name;
+		public GameObject prefab;
+	}
+	[ContextMenuItem("Generate", "Generate"), ContextMenuItem("Clear", "Clear"), Tooltip("How many tiles to place along each dimension")]
+	public TileSource[] tileSources;
+
 	[ContextMenuItem("Generate","Generate"), ContextMenuItem("Clear", "Clear"), Tooltip("How many units wide each tile (square) is")]
 	public float tileSize = 10;
 	[ContextMenuItem("Generate", "Generate"), ContextMenuItem("Clear", "Clear"), Tooltip("randomly rotate tile during random placement")]
-	public bool randZAngleOrthogonal = true;
-	[ContextMenuItem("Generate", "Generate"), ContextMenuItem("Clear", "Clear"), Tooltip("How many tiles to place along each dimension")]
-	public Vector3 tileAmount = new Vector3(10,1,10);
+	public bool randYAngleOrthogonal = true;
 
 	public GameObject[][][] generated = null;
 
-	private void OnValidate() {
-		// keep dimensions of the game world positive
-		for(int i = 0; i < 3; ++i) { tileAmount[i] = (int)Mathf.Max(1, tileAmount[i]); }
-	}
-
 	public GameObject GetNewRandomTile()
 	{
-		GameObject go = Instantiate(tiles[Random.Range(0, tiles.Length)]);
-		return go;
+		GameObject go = tileSources[Random.Range(0, tileSources.Length)].prefab;
+		return go ? Instantiate(go) : null;
 	}
 
 	public void Clear()
 	{
 		if(generated != null) {
-			for (int z = 0; z < tileAmount.z; ++z)
+			for (int z = 0; z < size.z; ++z)
 			{
-				for (int y = 0; y < tileAmount.y; ++y)
+				for (int y = 0; y < size.y; ++y)
 				{
-					for (int x = 0; x < tileAmount.z; ++z) {
+					for (int x = 0; x < size.z; ++z) {
 						if (!Application.isPlaying) {
 							DestroyImmediate(generated[z][y][z]);
 						} else {
@@ -60,23 +74,38 @@ public class TiledWorldGenerator : MonoBehaviour
 
 	public void Generate() {
 		Clear();
-		generated = new GameObject[(int)tileAmount.z][][];
-		for (int z = 0; z < tileAmount.z; ++z)
+		generated = new GameObject[size.z][][];
+		string correctedMap = map.Replace("\n", "").Replace("\r", "");
+		for (int z = 0; z < size.z; ++z)
 		{
-			generated[z] = new GameObject[(int)tileAmount.y][];
-			for (int y = 0; y < tileAmount.y; ++y)
+			generated[z] = new GameObject[size.y][];
+			for (int y = 0; y < size.y; ++y)
 			{
-				generated[z][y] = new GameObject[(int)tileAmount.x];
-				for (int x = 0; x < tileAmount.x; ++x)
+				generated[z][y] = new GameObject[size.x];
+				for (int x = 0; x < size.x; ++x)
 				{
-					GameObject go = GetNewRandomTile();
-					Transform t = go.transform;
-					t.SetParent(transform);
-					if (randZAngleOrthogonal)
-					{
-						t.Rotate(new Vector3(0, Random.Range(0, 4) * 90, 0));
+					GameObject go = null;
+					if (string.IsNullOrEmpty(map)) {
+						go = GetNewRandomTile();
+						Transform t = go.transform;
+						t.SetParent(transform);
+						if (randYAngleOrthogonal) {
+							t.Rotate(new Vector3(0, Random.Range(0, 4) * 90, 0));
+						}
+						t.localPosition = new Vector3(x, y, z) * tileSize;
+					} else {
+						int index = y * ((size.x) * size.z) + z * (size.x) + x;
+						char letter = correctedMap[index];
+						//Debug.Log(x+" "+y+" "+z+" : "+index+" '"+letter+"'");
+						TileSource ts = System.Array.Find(tileSources, t => t.name[0] == letter);
+						if(ts != null && ts.prefab != null) {
+							go = Instantiate(ts.prefab);
+							if (go) {
+								go.transform.SetParent(transform);
+								go.transform.localPosition = new Vector3(x, y, z) * tileSize;
+							}
+						}
 					}
-					t.localPosition = new Vector3(x, y, z) * tileSize;
 					generated[z][y][x] = go;
 				}
 			}
